bd410b87ccf43ce373d51e943e47449e90e6328d
Jmin0613 <jumin1356@gmail.com>
2026-07-10 01:16:56 +0900

    programmers Lv0 : 양꼬치, 짝수의 합

---
+++ src/LambSkewers.java
@@ -0,0 +1,5 @@
+public class LambSkewers {
+    public int solution(int n, int k) {
+        return 12000*n+2000*(k-n/10);
+    }
+}
---
+++ src/SumOfEvenNum.java
@@ -0,0 +1,12 @@
+public class SumOfEvenNum {
+    public int solution(int n) {
+        int answer = 0;
+        if(n%2!=0) n-=1;
+
+        while(n>0){
+            answer+=n;
+            n-=2;
+        }
+        return answer;
+    }
+}
