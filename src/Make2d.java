3e0861dc5e7d64eb8447b489e0966fc723b832ad
Jmin0613 <jumin1356@gmail.com>
2026-07-17 15:10:08 +0900

    programmers Lv.0 : 점의 ì위치 구하기, 2차원으로 만들기

---
+++ src/Make2d.java
@@ -0,0 +1,14 @@
+public class Make2d {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] result = new int[num_list.length/n][n];
+
+        int index = 0;
+        for(int i = 0; i< result.length; i++){
+            for(int j=0; j<n; j++){
+                result[i][j] = num_list[index++];
+            }
+        }
+
+        return result;
+    }
+}
